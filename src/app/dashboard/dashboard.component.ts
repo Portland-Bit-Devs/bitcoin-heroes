4ee1f1609ade07f76ae262c1ef7a64502f00a67e
Jon Austen <djangofan@gmail.com>
2023-04-22 13:17:25 -0700

    Save point.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,13 +8,18 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+
   heroes: Hero[] = [];
+
   constructor(private heroService: HeroService) { }
+
   ngOnInit(): void {
     this.getHeroes();
   }
+
   getHeroes(): void {
     this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes.slice(1, 5))
+      .subscribe(heroes => this.heroes = heroes)
   }
+
 }
